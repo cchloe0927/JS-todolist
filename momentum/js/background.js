bad5fb1043123fe97feca98d93df55cd492eb0a6
이현정 <96729804+cchloe09@users.noreply.github.com>
2022-02-18 16:33:58 +0900

    js

---
+++ momentum/js/background.js
@@ -0,0 +1,12 @@
+const images = [
+    "01.jpeg", "02.jpeg", "03.jpeg", "04.jpeg", "05.jpeg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+//js에서 html에 요소 추가하는 방법
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
---
+++ momentum/js/weather.js
@@ -0,0 +1,30 @@
+
+
+const API_KEY = "c8bf9b830ed849b23293b31574bd23b7";
+
+function onGeoOk(position) {
+    //콜백함수를 받아와서 퍼라미터(latitude, longitude)이용
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    //api로 위도 경도 숫자를 장소로 바꿔주는 url  //&units=metric: 섭씨로 단위변경 (url을 수정할 수 있음)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    //fetch로 url를 받아오고 then으로 응답을 받아옴
+    fetch(url).then(response => response.json()).then(data => {
+
+        const weater = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+
+        weater.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+    //에러시 띄어줄 메세지
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+//Geolocation.getCurrentPosition() 메서드는 장치의 현재 위치를 가져옵니다.
+//navigator.geolocation.getCurrentPosition(success, error)
